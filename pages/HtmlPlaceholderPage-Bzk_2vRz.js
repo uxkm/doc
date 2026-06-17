56f5199172031b9894d23aba406779b771421c6a
DeniceKim <18107365+DeniceKim@users.noreply.github.com>
2026-06-17 12:09:08 +0900

    빌드 후 테스트 검수대기 등록부분 수정

--- pages/HtmlPlaceholderPage-Bzk_2vRz.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./PublishingHtmlSlugRedirect-Dk12dCor.js";export{e as default};
---
+++ pages/HtmlPlaceholderPage-DqtI2Gda.js
@@ -0,0 +1,1 @@
+import{t as e}from"./PublishingHtmlSlugRedirect-B2YtAynF.js";export{e as default};
